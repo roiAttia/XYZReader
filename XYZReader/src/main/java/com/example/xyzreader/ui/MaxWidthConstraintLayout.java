afe1154dadd2072b79139ef6c5c167d622de0634
Roi Attia <roiattia.1@gmail.com>
2018-05-27 13:10:08 +0300

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/MaxWidthConstraintLayout.java
@@ -0,0 +1,43 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class MaxWidthConstraintLayout extends ConstraintLayout {
+
+    private static final int[] ATTRS = {
+            android.R.attr.maxWidth
+    };
+
+    private int mMaxWidth = Integer.MAX_VALUE;
+
+    public MaxWidthConstraintLayout(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public MaxWidthConstraintLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public MaxWidthConstraintLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle, 0);
+    }
+
+    public void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
+        mMaxWidth = a.getLayoutDimension(0, Integer.MAX_VALUE);
+        a.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int newSpecWidth = Math.min(MeasureSpec.getSize(widthMeasureSpec), mMaxWidth);
+        widthMeasureSpec = MeasureSpec.makeMeasureSpec(newSpecWidth, MeasureSpec.getMode(widthMeasureSpec));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
